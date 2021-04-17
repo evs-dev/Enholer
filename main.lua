1022274cd89688923bd170482721185cbf3d25e7
evs-dev <evansmeath@gmail.com>
2021-04-17 16:39:27 +0100

    Add basic enholing functionality

--- main.lua
+++ main.lua
@@ -7,3 +7,20 @@ function init()
 	--emit = false
 	--ui = false
 end
+
+function tick(dt)
+	-- Check if Enholer is selected
+	if GetString("game.player.tool") == "enholer" then
+		if GetBool("game.player.canusetool") and InputDown("lmb") then
+			local ct = GetCameraTransform();
+			local pos = ct.pos
+			local dir = TransformToParentVec(ct, Vec(0, 0, -1))
+			local hit, dist, normal, shape = QueryRaycast(pos, dir, 500)
+			if hit then
+				local hitPoint = VecAdd(pos, VecScale(dir, dist))
+    local radius = 0.4
+				MakeHole(hitPoint, radius, radius, radius)
+			end
+		end
+	end
+end
