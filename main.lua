b576b015a3fec5ab4cae254b4b846e0dc7a45426
evs-dev <evansmeath@gmail.com>
2021-04-17 16:30:38 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+function init()
+	RegisterTool("enholer", "Enholer", "MOD/vox/enholer.vox")
+	SetBool("game.tool.enholer.enabled", true)
+
+	--snd = LoadLoop("MOD/snd/gas.ogg")
+
+	--emit = false
+	--ui = false
+end
