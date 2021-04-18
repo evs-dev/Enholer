fb2885f84f5bc6ce3fbc25003cc6d2bbdf51fcc5
evs-dev <evansmeath@gmail.com>
2021-04-18 21:34:35 +0100

    Add radius changing interactive UI

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ function init()
  RADIUS_INCREMENT = 0.025
 
  radius = DEFAULT_RADIUS
+ inRadiusConfig = false
  hit = false
  hitPoint = Vec()
 end
@@ -22,14 +23,14 @@ function clamp(value, min, max)
 end
 
 function tick(dt)
- -- Check if Enholer is selected and can be used
- if GetString("game.player.tool") == "enholer" and GetBool("game.player.canusetool") then
+ -- Check if Enholer is selected
+ if GetString("game.player.tool") == "enholer" and (GetBool("game.player.canusetool") or inRadiusConfig) then
   -- Raycast at what the player is looking at, if anything
   local ct = GetCameraTransform()
   local pos = ct.pos
   local dir = TransformToParentVec(ct, Vec(0, 0, -1))
   local didHit, dist = QueryRaycast(pos, dir, 500)
-  
+
   -- Update the global variables for draw() to use
   hit = didHit
   if didHit then
@@ -37,17 +38,9 @@ function tick(dt)
   end
 
   -- Check for left click to make hole
-  if didHit and InputDown("lmb") then
+  if didHit and InputDown("lmb") and not inRadiusConfig then
    MakeHole(hitPoint, radius, radius, radius)
   end
-
-  -- Check for radius increasing/decreasing input
-  if InputDown("shift") then
-   radius = radius + RADIUS_INCREMENT
-  elseif InputDown("ctrl") then
-   radius = radius - RADIUS_INCREMENT
-  end
-  radius = clamp(radius, MIN_RADIUS, MAX_RADIUS)
  else
   hit = false
  end
@@ -57,10 +50,35 @@ function draw()
  if hit then
   -- Draw radius indicator scaled to world space radius
   local x, y, dist = UiWorldToPixel(hitPoint)
-  UiTranslate(x, y)
-  UiAlign("center middle")
-  UiColor(1, 1, 1, 0.7)
-  UiScale(10 * (radius / dist))
-  UiImage("circle.png")
+  UiPush()
+   UiTranslate(x, y)
+   UiAlign("center middle")
+   UiColor(1, 1, 1, 0.7)
+   UiScale(10 * (radius / dist))
+   UiImage("circle.png")
+  UiPop()
+ end
+
+ if InputPressed("r") then
+  inRadiusConfig = not inRadiusConfig
+ end
+
+ -- Radius configuration
+ if inRadiusConfig then
+  UiMakeInteractive()
+  UiTranslate(UiWidth() - 60, 60)
+  UiAlign("right middle")
+  UiFont("regular.ttf", 30)
+  UiText("Press A to decrease radius")
+  UiTranslate(0, 24)
+  UiText("Press D to increase radius")
+
+  if InputDown("a") then
+   radius = radius - RADIUS_INCREMENT
+  end
+  if InputDown("d") then
+   radius = radius + RADIUS_INCREMENT
+  end
+  radius = clamp(radius, MIN_RADIUS, MAX_RADIUS)
  end
 end
